e2b3eb0fa74e97cf3a51f0664ebe89c5302a5471
MohitKumarPro <mohit190497@gmail.com>
2025-07-24 02:52:34 +0530

    Added more enemies

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -1,11 +1,11 @@
 extends Camera2D
-
+@onready var hero =  $".."
 var shake_strength = 2
 var shake_time = 0.15
 var original_offset := Vector2.ZERO
 var shake_timer = 0.0
 var rng = RandomNumberGenerator.new()
-
+@onready var camera = $"."
 @onready var tween = get_tree().create_tween()
 
 func _ready():
@@ -16,7 +16,7 @@ func zoom_in_on_fire():
 	# Zoom in and out
 	tween.kill()
 	tween = get_tree().create_tween()
-	tween.tween_property(self, "zoom", Vector2(0.45, 0.45), 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "zoom", Vector2(0.48, 0.48), 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "zoom", Vector2(0.5, 0.5), 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 
 	# Start shake
@@ -24,12 +24,12 @@ func zoom_in_on_fire():
 	set_process(true)
 
 func _process(delta):
-	if shake_timer > 0:
-		shake_timer -= delta
-		offset = original_offset + Vector2(
-			rng.randf_range(-shake_strength, shake_strength),
-			rng.randf_range(-shake_strength, shake_strength)
-		)
-	else:
-		offset = original_offset
-		set_process(false)
+	
+	
+	var mouse_world_pos = get_global_mouse_position()
+	var player_pos = global_position
+	var direction_to_mouse = (mouse_world_pos - player_pos).normalized()
+	var camera_offset_strength = 300  # Increase for more forward visibility
+	var offset1 = direction_to_mouse * camera_offset_strength
+	offset = offset.lerp(offset1,delta*5)
+	original_offset = offset
